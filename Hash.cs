b4764964604ccb7f8d616a71637926555503c74d
Namchok Singhachai <44488002+namchokGithub@users.noreply.github.com>
2020-08-19 17:46:39 +0700

    update class hash

--- Hash.cs
+++ Hash.cs
@@ -1,6 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
 namespace C_sharp
 {
     public class Hash
     {
+        public string Salt { get; }
+        public string Digest { get; set; }
+
+        public Hash(string salt, string digest)
+        {
+            Salt = salt;
+            Digest = digest;
+        }
+    }
+
+
+    public class RNG
+    {
+        public string GenerateRandomCryptographicKey(int keyLength)
+        {
+            return Convert.ToBase64String(GenerateRandomCryptographicBytes(keyLength));
+        }
+
+        public byte[] GenerateRandomCryptographicBytes(int keyLength)
+        {
+            RNGCryptoServiceProvider rngCryptoServiceProvider = new RNGCryptoServiceProvider();
+            byte[] randomBytes = new byte[keyLength];
+            rngCryptoServiceProvider.GetBytes(randomBytes);
+            return randomBytes;
+        }
+    }
+
+    public class PasswordWithSaltHasher
+    {
+        public Hash HashWithSalt(string password, int saltLength, HashAlgorithm hashAlgo)
+        {
+            RNG rng = new RNG();
+            byte[] saltBytes = rng.GenerateRandomCryptographicBytes(saltLength);
+            byte[] passwordAsBytes = Encoding.UTF8.GetBytes(password);
+            List<byte> passwordWithSaltBytes = new List<byte>();
+            passwordWithSaltBytes.AddRange(passwordAsBytes);
+            passwordWithSaltBytes.AddRange(saltBytes);
+            byte[] digestBytes = hashAlgo.ComputeHash(passwordWithSaltBytes.ToArray());
+            return new Hash(Convert.ToBase64String(saltBytes), Convert.ToBase64String(digestBytes));
+        }
     }
 }
